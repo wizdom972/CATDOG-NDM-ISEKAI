a330b52a0b5df8a4097e47ea79a33bb78193251f
guderian2nd <gshin3@live.com>
2019-03-08 07:32:13 +0900

    fix design mismatch

--- ISEKAI/Assets/ISEKAI_Model/Events/Hunting_1.cs
+++ ISEKAI/Assets/ISEKAI_Model/Events/Hunting_1.cs
@@ -18,7 +18,7 @@ namespace ISEKAI_Model
 
         protected override bool exclusiveCondition()
         {
-            bool turnCondition = game.turn.totalMonthNumber >= 13 && game.turn.turnNumber <= 9;
+            bool turnCondition = game.turn.totalMonthNumber >= 13 && game.turn.totalMonthNumber <= 36;
             int chance = (new Random()).Next() / 10;
             bool chanceCondition = chance <= 1;
             if (_isFirstOccur && turnCondition)
